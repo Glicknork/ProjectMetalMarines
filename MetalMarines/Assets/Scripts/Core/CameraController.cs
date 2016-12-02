216de91b9e27de939da6e3f9a89b2514a8608f19
Glicknork <glicknork@hotmail.com>
2016-12-02 20:59:16 +0000

    Initial commit

---
+++ MetalMarines/Assets/Scripts/Core/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public float speed = 1f;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (Input.GetKey(KeyCode.W))
+        {            
+            transform.Translate(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector3.right * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed);
+        }
+
+    }
+}
